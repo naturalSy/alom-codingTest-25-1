b19a72f8c95a36647d0d8409393e218588e07f6d
naturalSy <chltpdbs6305@gmail.com>
2025-03-24 19:59:31 +0900

    합분해 / 30m

---
+++ week2/dynamic_programming/합분해/naturalSy/Main.java
@@ -0,0 +1,27 @@
+package dynamic_programming.합분해.naturalSy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        scanner.close();
+
+        int[][] dp = new int[k + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= k; i++) {
+            for (int j = 0; j <= n; j++) {
+                for (int l = 0; l <= j; l++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][l]) % 1000000000;
+                }
+            }
+        }
+
+        System.out.println(dp[k][n]);
+    }
+}
